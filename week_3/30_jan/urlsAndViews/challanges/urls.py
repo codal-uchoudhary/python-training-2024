f6610e27050fba356a261915c13ca1ab96da7170
Ummed <Uchoudhary@codal.com>
2024-01-30 18:43:17 +0530

    CIT-31 time 6h django prject based on views and urls

---
+++ week_3/30_jan/urlsAndViews/challanges/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns=[
+    path('',views.index),
+    path('<int:month>',views.monthly_challenge_by_int),
+    path('<str:month>',views.monthly_challenge,name='myUrl')
+]
+
+# reverse function
