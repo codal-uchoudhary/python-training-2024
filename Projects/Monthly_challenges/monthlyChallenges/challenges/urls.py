72878dd7056a6c43ed4d3f487896d69f6752c9e6
Ummed <Uchoudhary@codal.com>
2024-02-01 05:29:58 +0530

    CIT-31 #time 6h Learning Templates in django ( inheritance,tags and filter,include ,django template language,)

---
+++ Projects/Monthly_challenges/monthlyChallenges/challenges/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='mainurl'),
+    path('<int:month>',views.monthlyChallengeByInt),
+    path('<str:month>',views.monthlyChallenge,name='myUrl')
+]
