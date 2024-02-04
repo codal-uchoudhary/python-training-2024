9540afd548d7db60c220d12d4cfdd118f053ea82
Ummed <Uchoudhary@codal.com>
2024-02-04 11:52:16 +0530

    CIT-31 #time 1h 30m  insert and get the data in the Book model in bookoutlet app in Book_store project using python shell

--- Projects/Book_store/bookStore/bookOutlet/models.py
+++ Projects/Book_store/bookStore/bookOutlet/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating=models.IntegerField()
+    author= models.CharField(max_length=50,default=None)
+    is_bestselling = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} and rating is ({self.rating})"
