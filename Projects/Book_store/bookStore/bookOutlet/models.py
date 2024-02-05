b4c3bb8fccbd9c7b71ac991926162301b72f686a
Ummed <Uchoudhary@codal.com>
2024-02-05 18:36:11 +0530

    CIT-31 #time 3h create Author model in bookOutlet app and create one to many relation between Author model and Book model

--- Projects/Book_store/bookStore/bookOutlet/models.py
+++ Projects/Book_store/bookStore/bookOutlet/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating=models.IntegerField(default=1)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    author = models.CharField(null=True,max_length=50)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE,null=True)
     
 
     def __str__(self):
--- Projects/Book_store/bookStore/bookOutlet/views.py
+++ Projects/Book_store/bookStore/bookOutlet/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 
 from .models import Book
 # Create your views here.
+from django.db.models import Avg
+
+
 
 def index(request):
-    Books = Book.objects.all()
-    return render(request,'bookOutlet/index.html',{'Books':Books})
+    Books = Book.objects.all().order_by("price")
+    totalBooks = Books.count()
+    avg_rating= Books.aggregate(Avg("rating"))
+    return render(request,'bookOutlet/index.html',{'Books':Books,'count':totalBooks,"avgrating":avg_rating})
 
 def bookDetail(request,id):
-    Books = Book.objects.get(pk=id)
-    return(request,'bookOutlet/bookDetail.html')
+   
+    book = Book.objects.get(pk=id)
+    return render(request,'bookOutlet/bookDetail.html',{
+        'title':book.title,
+        'price':book.price,
+        'author':book.author,
+        'rating':book.rating
+    })
+    # return render(request,"bookOutlet/bookDetail.html")
