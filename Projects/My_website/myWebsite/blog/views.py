0a87b028fa4d500ce2ddf803a9e960642db9961b
Ummed <Uchoudhary@codal.com>
2024-02-13 10:40:45 +0530

    CIT-31 #time 6h implement authentication logic and creare 'Blog-create' template to create blog post

--- Projects/My_website/myWebsite/blog/views.py
+++ Projects/My_website/myWebsite/blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,redirect
 from datetime import date
-from . models import Post
-
+from . models import Post,Author
+from django.contrib import messages
 
 
 def startingPage(request):
@@ -18,4 +18,45 @@ def postDetail(request,slug):
     return render(request,"blog/postDetail.html",{'post':iPost,"post_tag":iPost.tags.all()})
 
 def createBlog(request):
-    return render(request,'blog/createBlog.html')
+    if request.method== 'POST':
+        title = request.POST['title']
+        excerpt = request.POST['excerpt']
+        content = request.POST['content']
+        tag = request.POST['tag']
+        image = request.POST['file']
+
+       
+        if request.user.is_authenticated:
+
+            first_name = request.user.first_name
+            last_name = request.user.last_name
+            email = request.user.email
+            author=None
+
+            if Author.objects.filter(email=email).exists():
+                author = Author.objects.filter(email=email).first()
+            else:
+                first_name = request.user.first_name
+                last_name = request.user.last_name
+                email = request.user.email
+                auth = Author(first_name = first_name,last_name=last_name,email = email)
+                auth.save()
+                author = Author.objects.filter(email=email).first()
+            
+            blog = Post(title=title,excerpt=excerpt,content=content,slug=title,image_name=image, author=author)
+            blog.save()
+            messages.success(request, "Blog is posted Sucessfuly")
+            return render(request,'blog/createBlog.html')
+
+
+        else:
+            messages.success(request, "Please Login")
+            return render(request,'blog/createBlog.html')
+
+
+
+
+
+    else:
+        return render(request,'blog/createBlog.html')
+   
--- Projects/My_website/myWebsite/myWebsite/settings.py
+++ Projects/My_website/myWebsite/myWebsite/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
         'PASSWORD': 'ummed',
         'HOST': 'localhost',
         'PORT': '5432'
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
