12277e5bbf2bc5bf9255699d1d2f95f482648d3b
Ummed <Uchoudhary@codal.com>
2024-02-26 18:49:16 +0530

    CIT-41 #time 3h Implement change password API and Logout API in the Blog app

--- Projects/My_website/myWebsite/account/views.py
+++ Projects/My_website/myWebsite/account/views.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from .serializers import *
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework import status
+
 
 # Create your views here.
 
@@ -37,3 +39,22 @@ class LoginApi(APIView):
                 return Response({'message':"user is not found"})
             token = Token.objects.get_or_create(user=user)
             return Response({'status':True,'message':"user is loged", 'token':str(token),'first_name':user.first_name,'email':user.email})
+
+class Logout(APIView):
+    def get(self,request):
+        request.user.auth_token.delete()
+        return Response({'message':"successfully logut"})
+    
+class ResetPassword(APIView):
+    def post(self,request):
+        serializers = ChangePasswordSerializer(data = request.data)
+        if serializers.is_valid():
+            user = request.user
+            if user.check_password(serializers.data.get('old_password')):
+                user.set_password(serializers.data.get('new_password'))
+                user.save()
+                return Response({'message':"password is changed"})
+            else:
+                return Response({'message':"old password is not correct"})
+        else:
+            return Response(serializers.errors)
--- Projects/My_website/myWebsite/blog/views.py
+++ Projects/My_website/myWebsite/blog/views.py
@@ -26,57 +26,16 @@ class userBlog(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get_item(self,id):
-        try:
-            item = Post.objects.get(id=id)
-            return item
-        except:
-            return False
-        
     def get(self,request):
         posts = Post.objects.filter(author=request.user.id)
         serializers = blogSerializer(posts,many=True)
         return Response(serializers.data)
     
-    def post(self,request):
-        data= request.data
-        data['author']=request.user.id
-        serializers = blogSerializer(data=data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data)
-        return Response(serializers.errors)
-    
-    def put(self,request,id):
-        obj = self.get_item(id=id)
-        if not obj: return Response({'message':"please enter valid blog-slug"})
-        if obj.author.id != request.user.id:
-            return Response({'message':"You are not authorised to perform this action"})
-        serializer = blogSerializer(obj,data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
-
-    
-    def patch(self,request,id):   
-        obj = self.get_item(id=id)
-        if not obj: return Response({'message':"please enter valid blog-slug"})
-        if obj.author.id != request.user.id:
-            return Response({'message':"You are not authorised to perform this action"})
-        if not obj: return Response({'message':"invalid blog-id"})
-        serializer = blogSerializer(obj,data=request.data,partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
- 
-    def delete(self,request,id):
-        obj = self.get_item(id=id)
-        if obj==False: return Response({'message':"please enter valid blog id"})
-        if obj.author.id != request.user.id:
-            return Response({'message':"You are not authorised to perform this action"})
-        obj.delete()
-        return Response({'message':"blog deleted"})
+    def put(self,request):
+        return Response({'message':"this is put method (under devlopment)"})
+    def delete(self,request):
+        return Response({'message':"this is get method (under devlopment)"})   
+    def patch(self,request):
+        return ({'message':"this is patch method (under devlopment)"})
     
     
