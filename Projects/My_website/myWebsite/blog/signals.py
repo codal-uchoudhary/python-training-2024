83abda509ee896db190f9136c726f4a1e2cae44d
Ummed <Uchoudhary@codal.com>
2024-03-06 19:04:15 +0530

    CIT-41 #time Learned signals - implement Model related signals in blog app. learned middleware

---
+++ Projects/My_website/myWebsite/blog/signals.py
@@ -0,0 +1,21 @@
+from django import dispatch
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from blog.models import Post
+from django.db.models.signals import pre_delete, post_save, post_delete
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+
+
+@receiver(post_save, sender=User)
+def action_user_login(sender, instance, **kwargs):
+    print(f"{instance.first_name} {instance.last_name} is regisered")
+
+
+@receiver(post_delete, sender=User)
+def action_user_login(sender, instance, **kwargs):
+    print(f"{instance.first_name} {instance.last_name} is deleted")
+
+
+@receiver(pre_delete, sender=Post)
+def action_user_login(sender, instance, **kwargs):
+    print(f"{instance.title} : blog is deleted")
