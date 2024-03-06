e43ed9476d7871297ed66b8e07593d4ffc85f16a
Ummed <Uchoudhary@codal.com>
2024-03-06 14:58:15 +0530

    CIT-41 #time 5m routers are updated in the blog app

--- Projects/My_website/myWebsite/blog/urls.py
+++ Projects/My_website/myWebsite/blog/urls.py
@@ -3,17 +3,11 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 from . import views
 from rest_framework import routers
 
-router = routers.SimpleRouter()
-router.register(r"myblog", views.myBlog)
 
-router2 = routers.SimpleRouter()
-router2.register(r"blog", views.blog)
-
-router_blog_app = routers.DefaultRouter()
-router_blog_app.extend(router)
-router_blog_app.extend(router2)
-
+router = routers.DefaultRouter()
+router.register("all-blogs", views.blog)
+router.register("my-blogs", views.myBlog)
 
 urlpatterns = [
-    path("", include(router_blog_app.urls)),
+    path("", include(router.urls)),
 ]
