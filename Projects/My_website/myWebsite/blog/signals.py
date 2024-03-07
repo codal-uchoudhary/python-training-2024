fe421b13bc32cfca1f67cc0b2adc942103f3346b
Ummed <Uchoudhary@codal.com>
2024-03-07 10:40:53 +0530

    CIT-41 #time 1h Explore Signal in detail and remove unwanted signals from blog app

--- Projects/My_website/myWebsite/blog/signals.py
+++ Projects/My_website/myWebsite/blog/signals.py
@@ -6,16 +6,11 @@ from django.db.models.signals import pre_delete, post_save, post_delete
 from django.contrib.auth.signals import user_logged_in, user_logged_out
 
 
-@receiver(post_save, sender=User)
-def action_user_login(sender, instance, **kwargs):
-    print(f"{instance.first_name} {instance.last_name} is regisered")
-
-
-@receiver(post_delete, sender=User)
-def action_user_login(sender, instance, **kwargs):
-    print(f"{instance.first_name} {instance.last_name} is deleted")
+@receiver(post_save, sender=Post)
+def post_save_receiver(sender, instance, **kwargs):
+    print(f"{instance.title} is saved")
 
 
 @receiver(pre_delete, sender=Post)
 def action_user_login(sender, instance, **kwargs):
-    print(f"{instance.title} : blog is deleted")
+    print(f"{instance.title} :blog is deleted ")
